d9e6e616bb23cad41d8cc84570663b80a890b7f0
10maurycy10 <10maurycy10@gmail.com>
2022-12-06 08:49:57 -0800

    fixed

--- 2022/day6/target/debug/day6.d
+++
@@ -1,1 +0,0 @@
-/home/mz/prog/aoc/2022/day6/target/debug/day6: /home/mz/prog/aoc/2022/day6/src/main.rs
--- 2022/day6/target/debug/deps/day6-86615f335ab8ed46.d
+++
@@ -1,5 +0,0 @@
-/home/mz/prog/aoc/2022/day6/target/debug/deps/day6-86615f335ab8ed46: src/main.rs
-
-/home/mz/prog/aoc/2022/day6/target/debug/deps/day6-86615f335ab8ed46.d: src/main.rs
-
-src/main.rs:
