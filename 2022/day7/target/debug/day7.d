84d8103708b34e4145c1a41f7b28638f9b68943a
10maurycy10 <10maurycy10@gmail.com>
2022-12-07 10:09:10 -0800

    ahhh

--- 2022/day7/target/debug/day7.d
+++
@@ -1,1 +0,0 @@
-/home/mz/prog/aoc/2022/day7/target/debug/day7: /home/mz/prog/aoc/2022/day7/src/main.rs
--- 2022/day7/target/debug/deps/day7-258cb00eb79c3958.d
+++
@@ -1,5 +0,0 @@
-/home/mz/prog/aoc/2022/day7/target/debug/deps/day7-258cb00eb79c3958: src/main.rs
-
-/home/mz/prog/aoc/2022/day7/target/debug/deps/day7-258cb00eb79c3958.d: src/main.rs
-
-src/main.rs:
